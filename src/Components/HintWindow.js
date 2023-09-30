4d5941e7add88fa44642e8d2444ee67b56ba2884
Abe <jmarkle@protonmail.com>
2023-09-30 14:19:02 -0400

    added hints window

---
+++ src/Components/HintWindow.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+
+function HintWindow({wordArrays, close, hintCount}) {
+
+    const [stringArray, setStringArray] = useState([])
+
+    useEffect(()=>{
+        createStrings()
+    },[])
+
+    // turns the array of array into an array of strings
+    function createStrings(){
+        if(typeof wordArrays !== "object"){
+            console.log("invalid word arrays: ")    
+            console.log(wordArrays)
+            return
+        }
+        // console.log(wordArrays)
+        // return
+
+        // The array that will hold all of the strings
+        let tempStringArray = []
+        // Look at each array of words
+        wordArrays.forEach(wordArray => {
+            // Add each word in the array to a string with a space between them
+            let tempString = ""
+            wordArray.forEach(word => {
+                tempString += word + " "
+            })
+            // Add this string of words to the array
+            tempStringArray.push(tempString)
+        })
+        // Put it in state to be displayed
+        setStringArray(tempStringArray)
+    }
+return (
+    <div className='window'>    
+        <div className='closeButton' onClick={close}>x</div>   
+        <div className='hintMenuInner'>
+            <div className='hintMenuLine'>{"There are "+hintCount+" hints remaining."}</div>
+            {hintCount >= 0 && stringArray.map(wordsString => (
+                <div className='hintMenuLine'>{wordsString}</div>
+            ))}
+        </div>                          
+    </div>
+  )
+}
+
+export default HintWindow
--- src/Games/WordArrayGame.js
+++ src/Games/WordArrayGame.js
@@ -1,12 +1,19 @@
 import { useState, useEffect, useRef } from 'react'
 import gear from '../images/gearicon80px.png'
 import Charts from '../Components/Charts'
+import wordFile from "../Files/1000.txt"
+import HintWindow from '../Components/HintWindow'
 
 function WordArrayGame() {
     
   // The 2d array of word arrays
   const [array, setArray] = useState([])  
+  // The array with the words to choose from  
+  const globalWordArray = useRef(["word"])
 
+  // Hint window
+  const [hintCount, setHintCount ]= useState(3)
+  const [showHint, setShowHint] = useState()
   // The current position in the current word array
   const [wordIndex, setWordIndex] = useState(0)
   // The current position in the 2d array
@@ -52,7 +59,7 @@ function WordArrayGame() {
 
   // Setup
   useEffect(()=>{
-    //createArray() 
+    createGlobalWordArray() 
     setInitialTime()   
     loadPoints()
   }, [])  
@@ -105,7 +112,16 @@ function WordArrayGame() {
     
   }
   function createArray(){
+        
+    // Create a new array and put 10 random words in it
+    var newWordsArray = []
+    for(var i=0; i<arrayLength; i++)
+      newWordsArray.push(globalWordArray.current[Math.floor(Math.random() * globalWordArray.current.length)])
 
+    // Adds the new word array at the head of the 2d array that holds word arrays
+    addAtHead(newWordsArray)
+  }  
+  function createGlobalWordArray(){
     // This is the list of words that the array will select from
     var words = `time year people way day man thing woman life child world school family student hand part place palace week company system program question work number night point home water room mother area money story fact month lot right study book eye job word business side kind head house service friend father hour game line member car city community Name team minute idea kid body information parent others level office door health person art history party result change morning reason research girl guy moment air teacher education car value gold baby food plant blue sun moon cloud trees plants electricity computer keyboard mouse book page word symbol hair ability time house water council market city land sea lake ocean sand rocks animals crab goat deer alligator bull team town nature bank paper pen marker club king voice light music field forest mountain valley peak project base love letter capital model machine fire son space plan energy hotel parkingLot meet cup box summer village park garden science picture fish bird oil film addition station window door sound glass software earth fiver sale equipment radio peace teacher culture context weight sex transport cash library phone stone dog cat memory railroad train plane sky wood granite marble winter snow rain hill wind bank museum farm cabinet fridge coffee tea bridge connection air dinner lunch breakfast fruit cantaloupe watermelon potato bright clear happy reach up climb progress grow accept accomplish achieved active`    
     
@@ -126,7 +142,7 @@ function WordArrayGame() {
     lizard
     monkey
     neon
-    octapus
+    octopus
     pussyCat
     queen
     riot
@@ -152,6 +168,922 @@ function WordArrayGame() {
     vine
     `
 
+    words +=
+    `
+      before
+      begin
+      behavior
+      behind
+      believe
+      benefit
+      best
+      better
+      between
+      beyond
+      big
+      bill
+      billion
+      bit
+      black
+      blood
+      blue
+      board
+      body
+      book
+      born
+      both
+      box
+      boy
+      break
+      bring
+      brother
+      budget
+      build
+      building
+      business
+      but
+      buy
+      by
+      call
+      camera
+      campaign
+      can
+      cancer
+      candidate
+      capital
+      car
+      card
+      care
+      career
+      carry
+      case
+      catch
+      cause
+      cell
+      center
+      central
+      century
+      certain
+      certainly
+      chair
+      challenge
+      chance
+      change
+      character
+      charge
+      check
+      child
+      choice
+      choose
+      church
+      citizen
+      city
+      civil
+      claim
+      class
+      clear
+      clearly
+      close
+      coach
+      cold
+      collection
+      college
+      color
+      come
+      commercial
+      common
+      community
+      company
+      compare
+      computer
+      concern
+      condition
+      conference
+      Congress
+      consider
+      consumer
+      contain
+      continue
+      control
+      cost
+      could
+      country
+      couple
+      course
+      court
+      cover
+      create
+      crime
+      cultural
+      culture
+      cup
+      current
+      customer
+      cut
+      dark
+      data
+      daughter
+      day
+      dead
+      deal
+      death
+      debate
+      decade
+      decide
+      decision
+      deep
+      defense
+      degree
+      Democrat
+      democratic
+      describe
+      design
+      despite
+      detail
+      determine
+      develop
+      development
+      die
+      difference
+      different
+      difficult
+      dinner
+      direction
+      director
+      discover
+      discuss
+      discussion
+      disease
+      do
+      doctor
+      dog
+      door
+      down
+      draw
+      dream
+      drive
+      drop
+      drug
+      during
+      each
+      early
+      east
+      easy
+      eat
+      economic
+      economy
+      edge
+      education
+      effect
+      effort
+      eight
+      either
+      election
+      else
+      employee
+      end
+      energy
+      enjoy
+      enough
+      enter
+      entire
+      environment
+      environmental
+      especially
+      establish
+      even
+      evening
+      event
+      ever
+      every
+      everybody
+      everyone
+      everything
+      evidence
+      exactly
+      example
+      executive
+      exist
+      expect
+      experience
+      expert
+      explain
+      eye
+      face
+      fact
+      factor
+      fail
+      fall
+      family
+      far
+      fast
+      father
+      fear
+      federal
+      feel
+      feeling
+      few
+      field
+      fight
+      figure
+      fill
+      film
+      final
+      finally
+      financial
+      find
+      fine
+      finger
+      finish
+      fire
+      firm
+      first
+      fish
+      five
+      floor
+      fly
+      focus
+      follow
+      food
+      foot
+      for
+      force
+      foreign
+      forget
+      form
+      former
+      forward
+      four
+      free
+      friend
+      from
+      front
+      full
+      fund
+      future
+      game
+      garden
+      gas
+      general
+      generation
+      get
+      girl
+      give
+      glass
+      go
+      goal
+      good
+      government
+      great
+      green
+      ground
+      group
+      grow
+      growth
+      guess
+      gun
+      guy
+      hair
+      half
+      hand
+      hang
+      happen
+      happy
+      hard
+      have
+      he
+      head
+      health
+      hear
+      heart
+      heat
+      heavy
+      help
+      her
+      here
+      herself
+      high
+      him
+      himself
+      his
+      history
+      hit
+      hold
+      home
+      hope
+      hospital
+      hot
+      hotel
+      hour
+      house
+      how
+      however
+      huge
+      human
+      hundred
+      husband
+      I
+      idea
+      identify
+      if
+      image
+      imagine
+      impact
+      important
+      improve
+      in
+      include
+      including
+      increase
+      indeed
+      indicate
+      individual
+      industry
+      information
+      inside
+      instead
+      institution
+      interest
+      interesting
+      international
+      interview
+      into
+      investment
+      involve
+      issue
+      it
+      item
+      its
+      itself
+      job
+      join
+      just
+      keep
+      key
+      kid
+      kill
+      kind
+      kitchen
+      know
+      knowledge
+      land
+      language
+      large
+      last
+      late
+      later
+      laugh
+      law
+      lawyer
+      lay
+      lead
+      leader
+      learn
+      least
+      leave
+      left
+      leg
+      legal
+      less
+      let
+      letter
+      level
+      lie
+      life
+      light
+      like
+      likely
+      line
+      list
+      listen
+      little
+      live
+      local
+      long
+      look
+      lose
+      loss
+      lot
+      love
+      low
+      machine
+      magazine
+      main
+      maintain
+      major
+      majority
+      make
+      man
+      manage
+      management
+      manager
+      many
+      market
+      marriage
+      material
+      matter
+      may
+      maybe
+      me
+      mean
+      measure
+      media
+      medical
+      meet
+      meeting
+      member
+      memory
+      mention
+      message
+      method
+      middle
+      might
+      military
+      million
+      mind
+      minute
+      miss
+      mission
+      model
+      modern
+      moment
+      money
+      month
+      more
+      morning
+      most
+      mother
+      mouth
+      move
+      movement
+      movie
+      Mr
+      Mrs
+      much
+      music
+      must
+      my
+      myself
+      name
+      nation
+      national
+      natural
+      nature
+      near
+      nearly
+      necessary
+      need
+      network
+      never
+      new
+      news
+      newspaper
+      next
+      nice
+      night
+      no
+      none
+      nor
+      north
+      not
+      note
+      nothing
+      notice
+      now
+      n't
+      number
+      occur
+      of
+      off
+      offer
+      office
+      officer
+      official
+      often
+      oh
+      oil
+      ok
+      old
+      on
+      once
+      one
+      only
+      onto
+      open
+      operation
+      opportunity
+      option
+      or
+      order
+      organization
+      other
+      others
+      our
+      out
+      outside
+      over
+      own
+      owner
+      page
+      pain
+      painting
+      paper
+      parent
+      part
+      participant
+      particular
+      particularly
+      partner
+      party
+      pass
+      past
+      patient
+      pattern
+      pay
+      peace
+      people
+      per
+      perform
+      performance
+      perhaps
+      period
+      person
+      personal
+      phone
+      physical
+      pick
+      picture
+      piece
+      place
+      plan
+      plant
+      play
+      player
+      PM
+      point
+      police
+      policy
+      political
+      politics
+      poor
+      popular
+      population
+      position
+      positive
+      possible
+      power
+      practice
+      prepare
+      present
+      president
+      pressure
+      pretty
+      prevent
+      price
+      private
+      probably
+      problem
+      process
+      produce
+      product
+      production
+      professional
+      professor
+      program
+      project
+      property
+      protect
+      prove
+      provide
+      public
+      pull
+      purpose
+      push
+      put
+      quality
+      question
+      quickly
+      quite
+      race
+      radio
+      raise
+      range
+      rate
+      rather
+      reach
+      read
+      ready
+      real
+      reality
+      realize
+      really
+      reason
+      receive
+      recent
+      recently
+      recognize
+      record
+      red
+      reduce
+      reflect
+      region
+      relate
+      relationship
+      religious
+      remain
+      remember
+      remove
+      report
+      represent
+      Republican
+      require
+      research
+      resource
+      respond
+      response
+      responsibility
+      rest
+      result
+      return
+      reveal
+      rich
+      right
+      rise
+      risk
+      road
+      rock
+      role
+      room
+      rule
+      run
+      safe
+      same
+      save
+      say
+      scene
+      school
+      science
+      scientist
+      score
+      sea
+      season
+      seat
+      second
+      section
+      security
+      see
+      seek
+      seem
+      sell
+      send
+      senior
+      sense
+      series
+      serious
+      serve
+      service
+      set
+      seven
+      several
+      sex
+      sexual
+      shake
+      share
+      she
+      shoot
+      short
+      shot
+      should
+      shoulder
+      show
+      side
+      sign
+      significant
+      similar
+      simple
+      simply
+      since
+      sing
+      single
+      sister
+      sit
+      site
+      situation
+      six
+      size
+      skill
+      skin
+      small
+      smile
+      so
+      social
+      society
+      soldier
+      some
+      somebody
+      someone
+      something
+      sometimes
+      son
+      song
+      soon
+      sort
+      sound
+      source
+      south
+      southern
+      space
+      speak
+      special
+      specific
+      speech
+      spend
+      sport
+      spring
+      staff
+      stage
+      stand
+      standard
+      star
+      start
+      state
+      statement
+      station
+      stay
+      step
+      still
+      stock
+      stop
+      store
+      story
+      strategy
+      street
+      strong
+      structure
+      student
+      study
+      stuff
+      style
+      subject
+      success
+      successful
+      such
+      suddenly
+      suffer
+      suggest
+      summer
+      support
+      sure
+      surface
+      system
+      table
+      take
+      talk
+      task
+      tax
+      teach
+      teacher
+      team
+      technology
+      television
+      tell
+      ten
+      tend
+      term
+      test
+      than
+      thank
+      that
+      the
+      their
+      them
+      themselves
+      then
+      theory
+      there
+      these
+      they
+      thing
+      think
+      third
+      this
+      those
+      though
+      thought
+      thousand
+      threat
+      three
+      through
+      throughout
+      throw
+      thus
+      time
+      to
+      today
+      together
+      tonight
+      too
+      top
+      total
+      tough
+      toward
+      town
+      trade
+      traditional
+      training
+      travel
+      treat
+      treatment
+      tree
+      trial
+      trip
+      trouble
+      true
+      truth
+      try
+      turn
+      TV
+      two
+      type
+      under
+      understand
+      unit
+      until
+      up
+      upon
+      us
+      use
+      usually
+      value
+      various
+      very
+      victim
+      view
+      violence
+      visit
+      voice
+      vote
+      wait
+      walk
+      wall
+      want
+      war
+      watch
+      water
+      way
+      we
+      weapon
+      wear
+      week
+      weight
+      well
+      west
+      western
+      what
+      whatever
+      when
+      where
+      whether
+      which
+      while
+      white
+      who
+      whole
+      whom
+      whose
+      why
+      wide
+      wife
+      will
+      win
+      wind
+      window
+      wish
+      with
+      within
+      without
+      woman
+      wonder
+      word
+      work
+      worker
+      world
+      worry
+      would
+      write
+      writer
+      wrong
+      yard
+      yeah
+      year
+      yes
+      yet
+      you
+      young
+      your
+      yourself
+    `
+
     words = words.replaceAll('\n'," ")
     words = words.replaceAll("  "," ")
     words = words.replaceAll("  "," ")
@@ -161,14 +1093,22 @@ function WordArrayGame() {
     var allWords = words.split(" ")
     allWords = allWords.filter(word => word.replaceAll(" ", "") !== "")
 
-    // Create a new array and put 10 random words in it
-    var newWordsArray = []
-    for(var i=0; i<arrayLength; i++)
-      newWordsArray.push(allWords[Math.floor(Math.random() * allWords.length)])
+    let fileReader = new FileReader()
+    // fileReader.onload(e => {
+    //   console.log(e)
+    //   console.log(fileReader.result)
+    // })
+    // fileReader.readAsText(wordFile)
+    // console.log(wordFile)
+    // console.log(wordFile.toString())
+    // let url = URL.createObjectURL(wordFile)
 
-    // Adds the new word array at the head of the 2d array that holds word arrays
-    addAtHead(newWordsArray)
-  }  
+    // fileReader.readAsDataURL(url)
+    
+    //fileReader.readAsDataURL(wordFile)
+
+    globalWordArray.current = allWords
+  }
   function debug(){
     console.log("________________________________________")
     console.log("array")
@@ -233,6 +1173,9 @@ function WordArrayGame() {
 
 
   }
+
+  const arrayDepthInput = useRef()
+  const arrayLengthInput = useRef()
   function saveSettings(){
     
     // Get input values
@@ -751,7 +1694,7 @@ function WordArrayGame() {
     lizard
     monkey
     neon
-    octapus
+    octopus
     pussyCat
     queen
     riot
@@ -778,6 +1721,13 @@ function WordArrayGame() {
       //alternator = !alternator
     //})
     //console.log(wordsNoNumbers)
+  }
+  function showHintFunction(){    
+
+    if(hintCount>0)
+      setHintCount(hintCount - 1)
+
+    setShowHint(true)
   }
 
   return (
@@ -871,12 +1821,17 @@ function WordArrayGame() {
             </div>            
           </div>
         </div> */}
+        {showHint && <HintWindow wordArrays={array} close={()=>setShowHint(false)} hintCount={hintCount}></HintWindow>}
+        
         {showChart && <Charts name={"Word Array Points"} dataArray={datePointArray} close={()=>setShowChart(false)}></Charts>}
         <div className='circleButtonHolder'>
           <div className='infoButton'>
             <img src={gear}></img>
             <div className='infoButtonDisplay'>
               <div className='settingsButton' onClick={()=>setShowChart(true)}>Points Chart</div>
+              <div className='settingsButton'>Settings</div>
+              <div className='settingsButton'>Description</div>
+              <div className='settingsButton' title={hintCount+" hints remaining"} onClick={showHintFunction}>Hint ({hintCount})</div>
             </div>
           </div>
         </div>
